5d2e9825c6b2b2c58c21d146abe6045524500a85
Stas <staszhingel@icloud.com>
2021-01-13 10:45:16 +0300

    Initial Commit

---
+++ identifible.ADDlist/identifible_ADDlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  identifible_ADDlistApp.swift
+//  identifible.ADDlist
+//
+//  Created by Стас Жингель on 13.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct identifible_ADDlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
